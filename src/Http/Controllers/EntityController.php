2567eeec59317530b1b122cf4a74d95736d92f04
vvvkor <vvvkor@users.noreply.github.com>
2018-05-07 12:29:29 +0300

    Fix: incorrect routes for entities with underscore in name

--- src/Http/Controllers/EntityController.php
+++ src/Http/Controllers/EntityController.php
@@ -14,7 +14,7 @@ abstract class EntityController extends CommonController
 		parent::__construct($repo, $model, $db);
 		//get table name from model class name
 		$m = explode('\\', get_class($model));
-		$this->entity = str_plural(strtolower(array_pop($m)));
+		$this->entity = snake_case(str_plural(array_pop($m)));
 		//or from url
 		//$this->entity = request()->segment(2);
 		////$this->entity = request()->segment(count(request()->segments()))
