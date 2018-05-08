3d914def785034076267b4b2c4bf5923c4097fc8
vvvkor <vvvkor@users.noreply.github.com>
2018-05-08 17:30:09 +0300

    Input type: time interval (HH:MM:SS)

--- src/resources/views/inputs/number.blade.php
+++ src/resources/views/inputs/number.blade.php
@@ -2,7 +2,11 @@
 	<label for="{{ $name }}" class="col-md-2 col-form-label">{{ $label }}</label>
 	<div class="col-md-10">
 		@php ( $val = old($name, $value) )
-		@php ( $val = strlen($val)>0 ? number_format(floatval($val), @$decimals ?: 0) : '' )
+		@if(strstr($val,':'))
+			@php( sscanf($val, "%d:%d:%d", $a, $b, $c) )
+			@php( $val = isset($c) ? $a*3600+$b*60+$c : $a*60+$b )
+		@endif
+		@php ( $val = strlen($val)>0 ? number_format(floatval($val), @$decimals ?: 0, '.', '') : '' )
 		<input class="form-control" {{ $order ? '' : 'autofocus' }} type="{{ $type  }}"
 			@if(@$decimals)
 				step="{{ pow(10, -$decimals) }}"
