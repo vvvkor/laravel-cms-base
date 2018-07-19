a54302e49e9d469abc1830392c5b43b95bf2433a
vvvkor <vadimkor@yandex.ru>
2018-07-19 15:06:20 +0300

    Vue component cleanup

--- src/resources/assets/js/app.js
+++ src/resources/assets/js/app.js
@@ -1,6 +1,12 @@
 //require('./bootstrap');
 //window.Vue = require('vue');
 
+//console.log('vue@cms');
+
+/*
+	//include this in root app.js (before new Vue)
+	require('./../vendor/cms/js/app');
+*/
+
 Vue.component('table-filter', require('./components/TableFilter.vue'));
 
-console.log('vue@cms');
